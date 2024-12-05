8359f2431c15fff35f0cc834947059a956abd8be
valsh <valsh@DESKTOP-7O5Q9S2>
2024-12-05 18:47:36 +0200

    Добавьте файлы проекта.

---
+++ lab_5 Шумаков В.Ю/lab_5 Шумаков В.Ю.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_CTYPE, "ukr");
+
+    int x, i, f, y;
+    cout << "Уведіть ціле число х: ";
+    cin >> x;
+
+    i = 1;
+    f = 1;
+
+    while (i <= x) {
+        f = f * i;
+        i = i + 1;
+    }
+
+    y = 2 * f;
+    cout << "y = " << y << '\n';
+
+    return 0;
+   
+}
